f93e69d06281288e576f5dfbee80bfb5cf2769ae
Tewodros Wakjira <getted87@gmail.com>
2013-02-19 11:21:37 +0300

    Added and modified script files

--- PharmInventory/Scripts/35_IssueDocDeletedInsert.sql
+++ PharmInventory/Scripts/35_IssueDocDeletedInsert.sql
@@ -1,1 +1,96 @@
-SELECT isnull(Cost,0) Cost, isnull(QuantityLeft,0) QuantityLeft, ib.*,  (isnull(Cost,0) * QuantityLeft) As Price FROM vwGetReceivedItemsByBatch ib WHERE ib.StoreId =8 AND (ib.ExpDate BETWEEN GETDATE() AND GETDATE() + 185 ) AND (ib.Out = 0) ORDER BY Price Desc
+USE [KOLFEHC_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[proc_IssueDocDeletedInsert]    Script Date: 1/13/2013 11:01:14 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[proc_IssueDocDeletedInsert]
+(
+	@ID int = NULL output,
+	@ItemID int = NULL,
+	@StoreId int = NULL,
+	@ReceivingUnitID int = NULL,
+	@LocalBatchNo varchar(50) = NULL,
+	@Quantity bigint = NULL,
+	@Date datetime = NULL,
+	@IsTransfer bit = NULL,
+	@IssuedBy varchar(50) = NULL,
+	@Remark text = NULL,
+	@RefNo varchar(50) = NULL,
+	@BatchNo varchar(50) = NULL,
+	@IsApproved bit = NULL,
+	@Cost float = NULL,
+	@NoOfPack int = NULL,
+	@QtyPerPack int = NULL,
+	@DUSOH bigint = NULL,
+	@DURequestedQty bigint = NULL,
+	@RecomendedQty bigint = NULL,
+	@RecievDocID int = NULL,
+	@EurDate datetime = NULL,
+	@OrderID int = NULL
+)
+AS
+BEGIN
+
+	SET NOCOUNT OFF
+	DECLARE @Err int
+
+	INSERT
+	INTO [IssueDocDeleted]
+	(
+	    [ID],
+		[ItemID],
+		[StoreId],
+		[ReceivingUnitID],
+		[LocalBatchNo],
+		[Quantity],
+		[Date],
+		[IsTransfer],
+		[IssuedBy],
+		[Remark],
+		[RefNo],
+		[BatchNo],
+		[IsApproved],
+		[Cost],
+		[NoOfPack],
+		[QtyPerPack],
+		[DUSOH],
+		[DURequestedQty],
+		[RecomendedQty],
+		[RecievDocID],
+		[EurDate],
+		[OrderID]
+	)
+	VALUES
+	(
+	    @ID,
+		@ItemID,
+		@StoreId,
+		@ReceivingUnitID,
+		@LocalBatchNo,
+		@Quantity,
+		@Date,
+		@IsTransfer,
+		@IssuedBy,
+		@Remark,
+		@RefNo,
+		@BatchNo,
+		@IsApproved,
+		@Cost,
+		@NoOfPack,
+		@QtyPerPack,
+		@DUSOH,
+		@DURequestedQty,
+		@RecomendedQty,
+		@RecievDocID,
+		@EurDate,
+		@OrderID
+			)
+	SET @Err = @@Error
+
+	SELECT @ID = SCOPE_IDENTITY()
+
+	RETURN @Err
+END
+
