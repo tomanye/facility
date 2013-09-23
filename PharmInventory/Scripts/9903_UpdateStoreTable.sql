9beaae8f6e6fc054ff9ad7da47492e6fa2880a06
Tewodros Wakjira <getted87@gmail.com>
2013-09-23 11:03:28 +0300

    HF-351

---
+++ PharmInventory/Scripts/9903_UpdateStoreTable.sql
@@ -0,0 +1,7 @@
+Update Stores
+Set SupplierID=1000,ReceivingUnitID=1000
+Where StoreName ='Budget store' and IsActive=1
+
+Update Stores
+set SupplierID=1001,ReceivingUnitID=1001
+where StoreName ='Program store' and IsActive =1
