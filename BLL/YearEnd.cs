cc23e1b9efc78f49aa097920f407ce1c008f0c5c
Tewodros Wakjira <getted87@gmail.com>
2015-07-21 12:04:14 +0300

    Inventory minor fix done

--- BLL/YearEnd.cs
+++ BLL/YearEnd.cs
@@ -80,9 +80,9 @@ namespace BLL
                                                       FROM      IssueDoc i
                                                       WHERE     CAST(YEAR(i.[Date]) as INT) = {0}
                                                     ) transact
-                                                    LEFT JOIN ( SELECT  *
+                                                    JOIN ( SELECT  *
                                                                 FROM    YearEnd yeInner
-                                                                WHERE   [Year] = {0} and AutomaticallyEntered = 0
+                                                                WHERE   [Year] = {0} and AutomaticallyEntered = 1
                                                               ) ye ON transact.StoreID = ye.StoreID AND transact.ItemID = ye.ItemID
                                                     JOIN Stores st ON transact.StoreID = st.ID
                                                     JOIN vwGetAllItems vw ON transact.ItemID = vw.ID
