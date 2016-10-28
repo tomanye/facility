4436cf365f5f34a62e2e4fb97e995583eb919371
Ehete Moges <eheteemm@gmail.com>
2016-10-28 09:39:06 +0300

    expired product 3 digit

--- PharmInventory/Forms/Reports/ExpiredProducts.Designer.cs
+++ PharmInventory/Forms/Reports/ExpiredProducts.Designer.cs
@@ -415,7 +415,7 @@ namespace PharmInventory.Forms.Reports
             // gridColumn6
             // 
             this.gridColumn6.Caption = "Expired Amount in Birr";
-            this.gridColumn6.DisplayFormat.FormatString = "#,##0.#0";
+            this.gridColumn6.DisplayFormat.FormatString = "n3";
             this.gridColumn6.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
             this.gridColumn6.FieldName = "Price";
             this.gridColumn6.Name = "gridColumn6";
