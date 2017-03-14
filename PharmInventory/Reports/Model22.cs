b7d01c17ed268f8d169b60e6225c79e368785b0f
Ehete Moges <eheteemm@gmail.com>
2017-03-14 15:59:42 +0300

    Model22 printout page added

---
+++ PharmInventory/Reports/Model22.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace PharmInventory.Reports
+{
+    public partial class Model22 : DevExpress.XtraReports.UI.XtraReport
+    {
+        
+        public Model22()
+        {
+            InitializeComponent();
+         }
+         
+    }
+}
