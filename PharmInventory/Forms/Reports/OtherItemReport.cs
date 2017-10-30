8fbab872bf297d1bed18d0d5b537f07547c07151
Ehete Moges <eheteemm@gmail.com>
2017-10-30 09:45:56 +0300

    Over Stock items header fix

--- PharmInventory/Forms/Reports/OtherItemReport.cs
+++ PharmInventory/Forms/Reports/OtherItemReport.cs
@@ -454,23 +454,26 @@ namespace PharmInventory.Forms.Reports
                     break;
                 case "Stock Out":
                     {
+                        string head = "Stocked Out Items Report";
+                        TextBrick brick = e.Graph.DrawString(head, Color.Black, new RectangleF(400, 40, 600, 100),
+                                                          BorderSide.None);
+                        brick.Font = new Font("Tahoma", 13);
                         string[] header =
-                            {
-                                "Stocked Out Items Report",
+                            { 
                                 info.HospitalName, "Date: " + dtDate.Text,
-                                "Store: " + cboStores.Text
+                                "Store: " + cboStores.Text,"Category: " + lkCommodityTypes.Text
                             };
                         printableComponentLink1.Landscape = true;
                         printableComponentLink1.PageHeaderFooter = header;
-
-                        TextBrick brick = e.Graph.DrawString(header[0], Color.DarkBlue, new RectangleF(0, 0, 200, 100),
-                                                             BorderSide.None);
-                        TextBrick brick1 = e.Graph.DrawString(header[1], Color.DarkBlue, new RectangleF(0, 20, 200, 100),
+                         
+                        TextBrick brick1 = e.Graph.DrawString(header[0], Color.DarkBlue, new RectangleF(0, 20, 200, 100),
                                                               BorderSide.None);
-                        TextBrick brick2 = e.Graph.DrawString(header[2], Color.DarkBlue, new RectangleF(0, 40, 200, 100),
+                        TextBrick brick2 = e.Graph.DrawString(header[1], Color.DarkBlue, new RectangleF(0, 40, 200, 100),
                                                               BorderSide.None);
-                        TextBrick brick3 = e.Graph.DrawString(header[3], Color.DarkBlue, new RectangleF(0, 60, 200, 100),
+                        TextBrick brick3 = e.Graph.DrawString(header[2], Color.DarkBlue, new RectangleF(0, 60, 200, 100),
                                                               BorderSide.None);
+                        TextBrick brick4 = e.Graph.DrawString(header[3], Color.DarkBlue, new RectangleF(0, 80, 200, 100),
+                                                             BorderSide.None); 
                     }
                     break;
             }
